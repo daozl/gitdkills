451e203ab3a6afdea1d62a9cb5a43dab81951961
zldao <zldao@linx-info.com>
2016-11-29 17:47:49 +0800

    touch client.c

---
+++ dao/client.c
@@ -0,0 +1,58 @@
+ /* tcpcli.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define DEFAULT_PORT 8800
+
+int main(int argc, char** argv)
+{
+    int cPort = DEFAULT_PORT;   //指定端口号
+    int cClient = 0;            
+    int cLen = 0;
+    struct sockaddr_in cli;       //定义client端地址
+    char cbuf[4096] = {0};
+    
+    if(argc < 2)
+    {
+        printf("Uasge: client[server IP address]\n");
+        return -1;
+    }
+    
+    memset(cbuf, 0, sizeof(cbuf));
+    
+    cli.sin_family = AF_INET;
+    cli.sin_port = htons(cPort);
+    cli.sin_addr.s_addr = inet_addr(argv[1]);
+    
+    cClient = socket(AF_INET, SOCK_STREAM, 0);
+    if(cClient < 0)
+    {
+        printf("socket() failure!\n");
+        return -1; 
+    }
+    
+    if(connect(cClient, (struct sockaddr*)&cli, sizeof(cli)) < 0)
+    {
+        printf("connect() failure!\n");
+        return -1;
+    }
+    
+    cLen = recv(cClient, cbuf, sizeof(cbuf),0);    
+    if((cLen < 0)||(cLen == 0))
+    {
+          printf("recv() failure!\n");
+        return -1;
+    }
+    printf("recv() Data From Server: [%s]\n", cbuf);
+    
+    close(cClient);
+    
+    return 0;
+}
