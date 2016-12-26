4d65e2b319344bcd65161575d8ad0003bd128d2c
zldao <zldao@linx-info.com>
2016-12-26 13:48:07 +0800

    modify run.sh add

--- inotify/run.sh
+++ inotify/run.sh
@@ -8,9 +8,10 @@ SRC_PATH="/opt/watch/$1"
 DES_PATH="/opt/linx_test/"
 mv $SRC_PATH $DES_PATH
 if [ $? -eq 0 ]; then
+	echo all > $DES_PATH/LINX_MOD_LIST
 	/opt/malware_sandbox_deploy/malware_app_env.sh -t 100 -p $DES_PATH/$1 -i 172.17.0.19 -o 8567
 fi
 
-rm $DES_PATH/$1 -r
+rm $DES_PATH/$1 -rf
 
 
